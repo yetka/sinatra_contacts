6d785de9d48a409b32cfbf3eba6efdf4259f9e06
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2017-11-30 10:00:06 -0800

    ad all, save, clear, find and sort method to test

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -5,6 +5,7 @@ class Contact
   attr_accessor :job_title
   attr_accessor :company
   attr_accessor :contact_type
+  attr_reader :id
 
 
   def initialize(attributes)
@@ -13,6 +14,34 @@ class Contact
     @job_title = attributes.fetch(:job_title)
     @company = attributes.fetch(:company)
     @contact_type = attributes.fetch(:contact_type)
+    @id = @@list.length + 1
   end
+
+  def self.all()
+    @@list
+  end
+
+  def save()
+    @@list.push(self)
+  end
+
+  def self.clear()
+    @@list = []
+  end
+
+  def self.find(id)
+    contact_id = id.to_i()
+    @@list.each do |contact|
+      if contact.id == contact_id
+        return contact
+      end
+    end
+  end
+
+
+  def self.sort()
+    @@list.sort_by{ |item| item.last_name }
+  end
+
 
 end
--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -2,11 +2,69 @@ require('rspec')
 require('contacts')
 
 describe("Contact") do
+  before() do
+    Contact.clear()
+  end
 
   describe("#first_name") do
     it("returns the first name of a contact") do
       new_contact = Contact.new({:first_name=> "Malgorzata", :last_name=> "Haniszewska", :job_title=> "DVM", :company=> "Azory", :contact_type=> "email"})
       expect(new_contact.first_name()).to(eq("Malgorzata"))
+    end
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Contact.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("saves new contact to the list of contacts") do
+      new_contact = Contact.new({:first_name=> "Malgorzata", :last_name=> "Haniszewska", :job_title=> "DVM", :company=> "Azory", :contact_type=> "email"})
+      new_contact.save()
+      expect(Contact.all()).to(eq([new_contact]))
+    end
+  end
+
+  describe(".clear") do
+    it("clears all contacts from the list") do
+      new_contact = Contact.new({:first_name=> "Malgorzata", :last_name=> "Haniszewska", :job_title=> "DVM", :company=> "Azory", :contact_type=> "email"})
+      new_contact.save()
+      Contact.clear()
+      expect(Contact.all()).to(eq([]))
+    end
+  end
+
+  describe("#id") do
+    it("increments an id by 1 each time a new new contact is added") do
+      new_contact = Contact.new({:first_name=> "Malgorzata", :last_name=> "Haniszewska", :job_title=> "DVM", :company=> "Azory", :contact_type=> "email"})
+      new_contact.save()
+      new_contact2 = Contact.new({:first_name=> "Tomasz", :last_name=> "Wiszkowski", :job_title=> "programmer", :company=> "Google", :contact_type=> "email"})
+      new_contact2.save()
+      expect(new_contact.id()).to(eq(1))
+      expect(new_contact2.id()).to(eq(2))
+    end
+  end
+
+  describe(".find") do
+    it("finds an new contact based on its id") do
+      new_contact1 = Contact.new({:first_name=> "Malgorzata", :last_name=> "Haniszewska", :job_title=> "DVM", :company=> "Azory", :contact_type=> "email"})
+      new_contact1.save()
+      new_contact2 = Contact.new({:first_name=> "Tomasz", :last_name=> "Wiszkowski", :job_title=> "programmer", :company=> "Google", :contact_type=> "email"})
+      new_contact2.save()
+      expect(Contact.find(1)).to(eq(new_contact1))
+      expect(Contact.find(2)).to(eq(new_contact2))
+    end
+  end
+
+  describe(".sort") do
+    it("sort the new_contacts based on their names") do
+      new_contact1 = Contact.new({:first_name=> "Tomasz", :last_name=> "Wiszkowski", :job_title=> "programmer", :company=> "Google", :contact_type=> "email"})
+      new_contact1.save()
+      new_contact2 = Contact.new({:first_name=> "Malgorzata", :last_name=> "Haniszewska", :job_title=> "DVM", :company=> "Azory", :contact_type=> "email"})
+      new_contact2.save()
+      expect(Contact.sort()).to(eq([new_contact2,new_contact1]))
     end
   end
 
